201abd595e2396feb93661d79bdd523d85844864
CyberShrek <illych.jr2@gmail.com>
2025-10-22 17:33:45 +0300

    Fix

--- src/main/java/org/vniizht/webapp_core/model/export/ExportReport.java
+++ src/main/java/org/vniizht/webapp_core/model/export/ExportReport.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExportReport {
-
     public String title;
     public final List<Section> context = new ArrayList<>();
     public final List<Table>   tables  = new ArrayList<>();
